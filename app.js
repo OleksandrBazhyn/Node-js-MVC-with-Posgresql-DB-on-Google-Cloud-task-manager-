8e98a6c377f47a6e490c36481502d1045c1ff504
OleksandrBazhyn <olexandrbazhyn@gmail.com>
2024-10-17 01:33:05 +0300

    Set up Express server with user and task routes

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const { initDb } = require('./models');
+const userRoutes = require('./routes/userRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+
+app.use('/users', userRoutes);
+app.use('/tasks', taskRoutes);
+
+initDb();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
