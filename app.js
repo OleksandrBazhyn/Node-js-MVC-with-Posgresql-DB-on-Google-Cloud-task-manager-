b04b0c87a7a302c6c4db512d71ef08a0b2070b66
OleksandrBazhyn <olexandrbazhyn@gmail.com>
2024-10-31 21:26:50 +0200

    Changed PORT for Azure Web App

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use('/attachments', attachmentRoutes);
 
 const startServer = async () => {
     await initDb();
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 8080;
     app.listen(PORT, () => {
         console.log(`Server is running on port: ${PORT}`);
     });
